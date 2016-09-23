5694773ce560bbdd5cba9ebca26e250d4c44b07c
Sharko-21 <Yarl9812@gmail.com>
2016-09-23 18:46:17 +0300

    second commit

---
+++ Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nerwork.Library;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Http;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nerwork.Library.client start = new Nerwork.Library.client();
+            Nerwork.Library.server open = new Nerwork.Library.server();
+
+            var ResponseArray = new string[3];
+            ResponseArray = start.Client();
+
+            open.Server(false, ResponseArray);
+            Console.Read();
+
+        }
+    }
+}
